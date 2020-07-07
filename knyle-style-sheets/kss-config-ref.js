08f55fd08cf39559eb64a905b2a2dc0855114bfb
Alexey Korablin <alex8456@bk.ru>
2020-07-07 23:23:21 +0300

    010_01 ch10 10.1.1 Setting up KSS

---
+++ knyle-style-sheets/kss-config-ref.js
@@ -0,0 +1,16 @@
+/*
+kss-config.json ref
+{
+  "title": "Pattern library",
+  "source": [
+    "./css"                   <-- путь к папке с CSS файлами
+  ],
+  "destibatiopn": "docs/",    <-- путь в котором будет сгенерирована библиотека
+  "css": [
+    "../css/styles.css"       <-- путь к стилям
+  ],
+  "js": [
+    "../js/docs.js"           <-- путь к скриптам
+  ]
+}
+*/
